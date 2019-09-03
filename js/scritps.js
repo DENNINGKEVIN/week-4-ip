0c559641ef68c2987db8a24821d1d674c51042fc
denningkevin <denningkev@gmail.com>
2019-09-03 22:03:31 +0300

    finalize changes

--- js/scritps.js
+++ js/scritps.js
@@ -6,26 +6,37 @@ function Order(name, pizza, topping, crust, boxes, delivery) {
   this.numberOfBoxes = boxes;
   this.deliveryOption = delivery;
 }
+
 function Address(city, estate, mobileNumber) {
   this.cityName = city;
   this.estateName = estate;
   this.customerMobile = mobileNumber;
 }
+
 function Price(pizzaPrice, toppingPrice, crustPrice) {
   this.priceForSelectedPizza = pizzaPrice;
   this.priceForSelectedTopping = toppingPrice;
   this.priceForSelectedCrust = crustPrice;
 }
+
+Price.prototype.subtotal = function () {
+  return this.priceForSelectedPizza + this.priceForSelectedTopping + this.priceForSelectedCrust;
+}
+
 function Bill(subtotal, boxes) {
   this.subtotalGotten = subtotal;
   this.orderedBoxes = boxes;
 }
-Price.prototype.subtotal = function () {
-  return this.priceForSelectedPizza + this.priceForSelectedTopping + this.priceForSelectedCrust;
-}
+
 Bill.prototype.total = function () {
   return this.subtotalGotten * this.orderedBoxes;
 }
+
+
+
+
+
+
 
 const navSlide = () => {
   const burger = document.querySelector('.burger');
@@ -53,35 +64,38 @@ const navSlide = () => {
     moto.classList.toggle('moto-unseen');
   });
 }
+
 navSlide();
-unction switchTopping() {
+
+
+function switchTopping() {
   if (document.getElementById('toppings')) {
 
-      if (document.getElementById('toppings').style.display == 'none') {
-          document.getElementById('toppings').style.display = 'block';
-          document.getElementById('classic').style.display = 'none';
-          document.getElementById('deluxe').style.display = 'none';
-      }
-      else {
-          document.getElementById('toppings').style.display = 'block';
-          document.getElementById('classic').style.display = 'none';
-          document.getElementById('deluxe').style.display = 'none';
-      }
-    }
-  }
-
-function switchClassic() {
-if (document.getElementById('classic')) {
-
-    if (document.getElementById('classic').style.display == 'none') {
-        document.getElementById('toppings').style.display = 'none';
-        document.getElementById('classic').style.display = 'block';
-        document.getElementById('deluxe').style.display = 'none';
+    if (document.getElementById('toppings').style.display == 'none') {
+      document.getElementById('toppings').style.display = 'block';
+      document.getElementById('classic').style.display = 'none';
+      document.getElementById('deluxe').style.display = 'none';
     }
     else {
-        document.getElementById('toppings').style.display = 'none';
-        document.getElementById('classic').style.display = 'block';
-        document.getElementById('deluxe').style.display = 'none';
+      document.getElementById('toppings').style.display = 'block';
+      document.getElementById('classic').style.display = 'none';
+      document.getElementById('deluxe').style.display = 'none';
+    }
+  }
+}
+
+function switchClassic() {
+  if (document.getElementById('classic')) {
+
+    if (document.getElementById('classic').style.display == 'none') {
+      document.getElementById('toppings').style.display = 'none';
+      document.getElementById('classic').style.display = 'block';
+      document.getElementById('deluxe').style.display = 'none';
+    }
+    else {
+      document.getElementById('toppings').style.display = 'none';
+      document.getElementById('classic').style.display = 'block';
+      document.getElementById('deluxe').style.display = 'none';
     }
   }
 }
@@ -89,22 +103,22 @@ if (document.getElementById('classic')) {
 function switchDeluxe() {
   if (document.getElementById('deluxe')) {
 
-      if (document.getElementById('deluxe').style.display == 'none') {
-          document.getElementById('toppings').style.display = 'none';
-          document.getElementById('classic').style.display = 'none';
-          document.getElementById('deluxe').style.display = 'block';
-      }
-      else {
-          document.getElementById('toppings').style.display = 'none';
-          document.getElementById('classic').style.display = 'none';
-          document.getElementById('deluxe').style.display = 'block';
-      }
+    if (document.getElementById('deluxe').style.display == 'none') {
+      document.getElementById('toppings').style.display = 'none';
+      document.getElementById('classic').style.display = 'none';
+      document.getElementById('deluxe').style.display = 'block';
+    }
+    else {
+      document.getElementById('toppings').style.display = 'none';
+      document.getElementById('classic').style.display = 'none';
+      document.getElementById('deluxe').style.display = 'block';
     }
   }
+}
 
 
-$(document).ready(function(){
-  $('form#new-order').submit(function(event){
+$(document).ready(function () {
+  $('form#new-order').submit(function (event) {
     event.preventDefault();
 
 
@@ -118,7 +132,7 @@ $(document).ready(function(){
 
     var crusts = ['Thick', 'Thin'];
     var toppings = ['Pepperoni', 'Mushroom', 'Sausage', 'Onions', 'Bacon', 'Chicken'];
-    var pizzas = ['Veg Tikka: SMALL', 'Veg Tikka: MEDIUM', 'Veg Tikka: LARGE', 'Chicken Tikka: SMALL', 'Chicken Tikka: MEDIUM', 'Chicken Tikka: LARGE', 'Peri Peri Chicken: SMALL', 'Peri Peri Chicken: MEDIUM', 'Peri Peri Chicken: LARGE', 'BBQ Steak: SMALL', 'BBQ Steak: MEDIUM', 'BBQ Steak: LARGE', 'Boerewors: SMALL', 'Boerewors: MEDIUM', 'Boerewors: LARGE', 'Hawaiian: SMALL', 'Hawaiian: MEDIUM', 'Hawaiian: LARGE' ];
+    var pizzas = ['Veg Tikka: SMALL', 'Veg Tikka: MEDIUM', 'Veg Tikka: LARGE', 'Chicken Tikka: SMALL', 'Chicken Tikka: MEDIUM', 'Chicken Tikka: LARGE', 'Peri Peri Chicken: SMALL', 'Peri Peri Chicken: MEDIUM', 'Peri Peri Chicken: LARGE', 'BBQ Steak: SMALL', 'BBQ Steak: MEDIUM', 'BBQ Steak: LARGE', 'Boerewors: SMALL', 'Boerewors: MEDIUM', 'Boerewors: LARGE', 'Hawaiian: SMALL', 'Hawaiian: MEDIUM', 'Hawaiian: LARGE'];
     var deliveryOptions = ['Collect', 'Deliver'];
 
     var pizzaPrices = [400, 600, 1000, 600, 800, 1200, 650, 850, 1250, 500, 700, 1100, 550, 750, 1150, 700, 900, 1300];
@@ -126,14 +140,14 @@ $(document).ready(function(){
     var crustPrices = [200, 0];
 
 
-    var priceOfSelectedPizza = pizzaPrices[selectedPizza-1];
+    var priceOfSelectedPizza = pizzaPrices[selectedPizza - 1];
     var priceOfToppingSelected = toppingPrices[toppingSelected];
-    var priceOfCrustSelected = crustPrices[crustSelected-1];
+    var priceOfCrustSelected = crustPrices[crustSelected - 1];
 
-    var nameOfPizza = pizzas[selectedPizza-1];
-    var nameOfTopping = toppings[toppingSelected-1];
-    var nameOfCrust = crusts[crustSelected-1];
-    var nameOfDeliveryOption = deliveryOptions[selectedDeliveryOption-1];
+    var nameOfPizza = pizzas[selectedPizza - 1];
+    var nameOfTopping = toppings[toppingSelected - 1];
+    var nameOfCrust = crusts[crustSelected - 1];
+    var nameOfDeliveryOption = deliveryOptions[selectedDeliveryOption - 1];
 
 
     var newOrder = new Order(inputtedName, nameOfPizza, nameOfTopping, nameOfCrust, noOfBoxes, nameOfDeliveryOption);
@@ -144,22 +158,22 @@ $(document).ready(function(){
 
 
 
-    if (selectedDeliveryOption == "2"){
+    if (selectedDeliveryOption == "2") {
       $('#address').show();
 
     }
-    else{
+    else {
       document.getElementById('order-summary').value = newOrder.selectedPizza + ' * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedPizza * newOrder.numberOfBoxes + '\n' + newOrder.selectedTopping + ' topping * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedTopping * newOrder.numberOfBoxes + '\n' + newOrder.selectedCrust + ' crust * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedCrust * newOrder.numberOfBoxes + '\n' + 'Total: ' + newTotal;
     }
 
-    $('form#delivery-address').submit(function(event){
+    $('form#delivery-address').submit(function (event) {
       event.preventDefault();
 
 
       document.getElementById('order-summary').value = newOrder.selectedPizza + ' * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedPizza * newOrder.numberOfBoxes + '\n' + newOrder.selectedTopping + ' topping * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedTopping * newOrder.numberOfBoxes + '\n' + newOrder.selectedCrust + ' crust * ' + newOrder.numberOfBoxes + ' = ' + newPrices.priceForSelectedCrust * newOrder.numberOfBoxes + '\n' + 'Total: ' + newTotal;
     });
 
-    $('form#checkout-form').submit(function(event){
+    $('form#checkout-form').submit(function (event) {
       event.preventDefault();
 
       var inputtedCity = $('input#city').val();
@@ -168,12 +182,12 @@ $(document).ready(function(){
 
       var newAddress = new Address(inputtedCity, inputtedEstate, inputtedNumber);
 
-      if(selectedDeliveryOption == "2"){
+      if (selectedDeliveryOption == "2") {
 
         alert(newOrder.customerName + '. Your order will be delivered to ' + newAddress.estateName + ', ' + newAddress.cityName + ' in the next 30 minutes. We will call this number (' + newAddress.customerMobile + ') when we get there. Thank you for being a loyal l-unico customer');
       }
-      else{
-        alert(newOrder.customerName +', your order will be ready for collection in 25 minutes.  Thank you for being a loyal l-unico customer');
+      else {
+        alert(newOrder.customerName + ', your order will be ready for collection in 25 minutes.  Thank you for being a loyal l-unico customer');
       }
 
       document.getElementById("checkout-form").reset();
@@ -183,4 +197,3 @@ $(document).ready(function(){
     });
   });
 });
-
