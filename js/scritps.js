0e051c6627e09b05eab3d15ac38ac26cd53aae9d
denningkevin <denningkev@gmail.com>
2019-09-03 22:00:40 +0300

    add toggle function to website

--- js/scritps.js
+++ js/scritps.js
@@ -6,23 +6,50 @@ function Order(name, pizza, topping, crust, boxes, delivery) {
   this.numberOfBoxes = boxes;
   this.deliveryOption = delivery;
 }
-function Address(city, estate, mobileNumber){
+function Address(city, estate, mobileNumber) {
   this.cityName = city;
   this.estateName = estate;
   this.customerMobile = mobileNumber;
 }
-function Price(pizzaPrice, toppingPrice, crustPrice){
+function Price(pizzaPrice, toppingPrice, crustPrice) {
   this.priceForSelectedPizza = pizzaPrice;
   this.priceForSelectedTopping = toppingPrice;
   this.priceForSelectedCrust = crustPrice;
 }
-function Bill(subtotal, boxes){
+function Bill(subtotal, boxes) {
   this.subtotalGotten = subtotal;
   this.orderedBoxes = boxes;
 }
-Price.prototype.subtotal = function(){
+Price.prototype.subtotal = function () {
   return this.priceForSelectedPizza + this.priceForSelectedTopping + this.priceForSelectedCrust;
 }
-Bill.prototype.total = function(){
+Bill.prototype.total = function () {
   return this.subtotalGotten * this.orderedBoxes;
+}
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navi');
+  const navLinks = document.querySelectorAll('.navi li');
+  const moto = document.querySelector('#moto');
+  const history = document.querySelector('.history');
+  const aboutTitle = document.querySelector('.about-title');
+  const forms = document.querySelector('#forms');
+
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('navi-active');
+
+    navLinks.forEach((navLink, index) => {
+      if (navLink.style.animation) {
+        navLink.style.animation = ``;
+      }
+      else {
+        navLink.style.animation = `naviFade 0.5s ease forwards ${index / 7 + 1}s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+
+    moto.classList.toggle('moto-unseen');
+  });
 }
